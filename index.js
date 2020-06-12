7d2c53fa16be9a5621999ffa4ce3ada8928f47ce
Alex Bruns <ahb@Alexs-MacBook-Pro.local>
2020-06-12 15:32:48 -0500

    AHB

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
+const { spawn } = require("child_process");
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  const runner = spawn("node", ["index.js"]);
+  runner.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  runner.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  runner.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
